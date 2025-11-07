2b2fe5f139dac3fdde017d6e07e701ba71e3a414
lucdev <me@lucdev.net>
2025-11-07 18:33:47 +0000

    frontend: configure base path

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
-const basePath = "/landing-bay-rl";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
   reactCompiler: true,
-  basePath,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: `${basePath}/`,
-  },
+  basePath: "/landing-bay-rl",
 };
 
 export default nextConfig;
